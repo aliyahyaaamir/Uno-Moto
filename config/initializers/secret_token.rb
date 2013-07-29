06a84f6f132b5d817d9c07a95ae98e85d916ee43
ali aamir <ali_aamir@hotmail.com>
2013-07-29 17:55:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnoMoto::Application.config.secret_token = '330541b4107628025aab6b420370899e43f019eb06124325472f85e95500501278bbd720dc94ec307a3fabb1b16ea1badeb7c80c082fe630dcf9788ebeb1447d'
