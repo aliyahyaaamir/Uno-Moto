be0cd9467b22435eaad5e66e91165f1797a29fdd
ali aamir <ali_aamir@hotmail.com>
2013-07-31 17:05:17 -0400

    Made tests for user sign up nav pass

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ UnoMoto::Application.routes.draw do
 
 root :to => "landing#index"
 
+resources :sessions
+
 end
--- spec/requests/user_sign_ups_spec.rb
+++ spec/requests/user_sign_ups_spec.rb
@@ -5,7 +5,8 @@ describe "UserSignUps" do
 		visit '/'
 		# user = FactoryGirl.create(:user)
 		assert find('#menu').has_link?('Sign Up')
-		click_link "Sign Up"
+		
+		find('#mainNav').click_link('Sign Up')
 		assert_equal new_session_path, current_path
 	end
 
