105b06e17e61b4678011f8747d67012cf889ae9e
ali aamir <ali_aamir@hotmail.com>
2013-07-31 12:33:41 -0400

    Setting up rspec for testing

--- Gemfile
+++ Gemfile
@@ -17,9 +17,12 @@ group :development do
 	gem 'pry-rails'
 end
 
+gem "rspec-rails", :group => [:test, :development]
+
 group :test do
 	gem "factory_girl_rails"
 	gem "capybara"
+	gem "guard-rspec"
 end
 
 
--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module UnoMoto
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.generators do |g|
-        g.test_framework :test_unit, :fixture => false
-        
+        g.test_framework :rspec, :fixture => false
+    end
   end
 end
