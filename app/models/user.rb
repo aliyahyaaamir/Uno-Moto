f413fbbc9db9a7b010b6de6275e83a9ab2ed6494
ali aamir <ali_aamir@hotmail.com>
2013-08-01 14:03:42 -0400

    Added password_confirmation field to view and user model, as well as a username field in the view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  attr_accessible :first_name, :last_name, :user_name, :password
+  attr_accessible :first_name, :last_name, :user_name, :password, :password_confirmation
   validates_presence_of :password, :on => :create
   validates :first_name, :last_name, :email, :username, :presence => true
 
