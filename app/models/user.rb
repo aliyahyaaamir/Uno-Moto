4d563040a42119eb066246f5ab868c4d59c7dbe6
ali aamir <ali_aamir@hotmail.com>
2013-08-01 13:49:23 -0400

    Made User model validation tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  # attr_accessible :title, :body
+  attr_accessible :first_name, :last_name, :user_name, :password
+  validates_presence_of :password, :on => :create
+  validates :first_name, :last_name, :email, :username, :presence => true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,18 +5,18 @@ describe User do
 		FactoryGirl.create(:user).should be_valid
 	end
 	it "is invalid without a firstname" do
-		FactoryGirl.create(:user, first_name: nil).should_not be_valid
+		FactoryGirl.build(:user, first_name: nil).should_not be_valid
 	end
 	it "is invalid without a lastname" do
-		FactoryGirl.create(:user, last_name: nil).should_not be_valid
+		FactoryGirl.build(:user, last_name: nil).should_not be_valid
 	end
 	it "is invalid without a username" do
-		FactoryGirl.create(:user, username: nil).should_not be_valid
+		FactoryGirl.build(:user, username: nil).should_not be_valid
 	end
 	it "is invalid without an email" do
-		FactoryGirl.create(:user, email: nil).should_not be_valid
+		FactoryGirl.build(:user, email: nil).should_not be_valid
 	end
 	it "is invalid without a password" do
-		FactoryGirl.create(:user, password: nil).should_not be_valid
+		FactoryGirl.build(:user, password: nil).should_not be_valid
 	end
 end
