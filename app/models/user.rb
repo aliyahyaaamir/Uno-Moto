122965fd47a8d2a538098d39eee40457c62d60eb
ali aamir <ali_aamir@hotmail.com>
2013-08-01 16:10:38 -0400

    Wrote test for user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   attr_accessible :first_name, :last_name, :username, :password, :password_confirmation, :email
+  validates :first_name, :last_name, :email, :username, :presence => true
+  validates_length_of :password, :minimum => 5, :message => "password must be at least 5 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
   validates_presence_of :password, :on => :create
-  validates :first_name, :last_name, :email, :username, :presence => true
 
 end
---
+++ spec/requests/test_logins_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "TestLogins" do
+	it "checks to see that a user is able to login" do
+		pass = "this-is-a-password"
+		user = FactoryGirl.create :user, password: pass
+		visit '/sessions/new'
+
+		fill_in "email", with: user.email
+		fill_in "password", with: pass
+		click_button "Login"
+	end
+end
