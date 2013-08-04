416593a0642015ed063b2fd2f39a05fa21d0ea7f
ali aamir <ali_aamir@hotmail.com>
2013-08-03 23:52:06 -0400

    Made tests for rental item model pass and corrected typo in test

--- app/models/rental_item.rb
+++ app/models/rental_item.rb
@@ -1,4 +1,5 @@
 class RentalItem < ActiveRecord::Base
   belongs_to :user
   attr_accessible :title, :description, :user_id, :price_per_day, :price_per_week
+  validates :title, :description, :price_per_day, :price_per_week, :presence => true
 end
