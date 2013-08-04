8927d4189830ee39f3a81eb2dc0cee049222ac71
ali aamir <ali_aamir@hotmail.com>
2013-08-03 20:11:51 -0400

    Cleaned up nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  has many :rental_items
   attr_accessible :first_name, :last_name, :password, :password_confirmation, :email, :username
   validates :first_name, :last_name, :email,:username, :presence => true
   validates_length_of :password, :minimum => 5, :message => "password must be at least 5 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
   validates_presence_of :password, :on => :create
+  # has many :rental_items
 
 end
