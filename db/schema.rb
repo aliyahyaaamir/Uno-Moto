b39e0dcbb218d393c0e4ef98a73a06fffb546e70
ali aamir <ali_aamir@hotmail.com>
2013-08-07 12:00:53 -0400

    Created Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130806200558) do
+ActiveRecord::Schema.define(:version => 20130807154350) do
+
+  create_table "bookings", :force => true do |t|
+    t.time     "time"
+    t.date     "date"
+    t.integer  "user_id"
+    t.integer  "rental_item_id"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
   create_table "locations", :force => true do |t|
     t.string   "address"
